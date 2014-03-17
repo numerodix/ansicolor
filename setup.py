5f6bcd4084e4deed94e4102ebe6b438243f19314
Martin Matusiak <numerodix@gmail.com>
2014-03-17 19:44:04 +0100

    flake8 failing on py34, removing

--- setup.py
+++ setup.py
@@ -18,14 +18,15 @@ setup(
         'ansicolor',
     ],
     classifiers=[
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
-        'License :: OSI Approved :: Apache Software License',
     ],
     # don't install as zipped egg
     zip_safe=False,
