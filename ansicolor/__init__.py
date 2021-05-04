11c4edb48827fe2935a3804d77013adbfa0027c6
Martin Matusiak <numerodix@gmail.com>
2021-05-05 09:37:23 +1000

    bump version

--- ansicolor/__init__.py
+++ ansicolor/__init__.py
@@ -31,6 +31,6 @@ __all__ = [
     'Colors',
 ]
 
-__major_version__ = "0.2"
-__release__ = "4"
+__major_version__ = "0.3"
+__release__ = "0"
 __version__ = "%s.%s" % (__major_version__, __release__)
